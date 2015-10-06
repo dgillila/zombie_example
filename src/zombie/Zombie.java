2cc93fb78ace5501b1cd42485bcfb2a3101e367d
Daniel <Daniel@192.168.1.132>
2015-10-05 18:46:35 -0600

    Added 'new' to output

--- src/zombie/Zombie.java
+++ src/zombie/Zombie.java
@@ -16,7 +16,7 @@ public class Zombie {
      */
     public static void main(String[] args) {
         
-        System.out.println("Hello world.");
+        System.out.println("Hello new world.");
         
     }
     
